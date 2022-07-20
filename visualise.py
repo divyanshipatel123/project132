6852d07afb0aae897ffa4860df239c405cfc9e44
divyanshipatel123 <divyanshipatel2008apr13@gmail.com>
2022-07-20 15:37:08 +0530

    files

--- visualise.py
+++ visualise.py
@@ -4,14 +4,6 @@ import plotly.express as px
 import numpy as np
 df = pd.read_csv("final_gravity.csv")
 
-masses = df["Star_Mass"].tolist()
-radiuses = df["Star_Radiuses"].tolist()
-gravity = df["Star_Gravity"].tolist()
-for i , data in enumerate(masses):
-    masses[i] = float(data)/1.989e+30
-for i , data in enumerate(radiuses):
-    radiuses[i] = float(data)/6.957e+8
-
 fig  = px.scatter(df , x =  "Star_Radiuses", y = "Star_Mass" , title = "Radius Vs Mass graph")
 fig.show()
 fig2  = px.scatter(df , x =  "Star_Mass", y = "Star_Gravity" , title = "Radius Vs Mass graph")
